2b61d547f04e11c4e291b266ded7af03c6ca49c4
SnehaPant <72789523+SnehaPant@users.noreply.github.com>
2021-02-04 08:59:30 +0530

    Add files via upload

---
+++ power.js
@@ -0,0 +1,22 @@
+class Power{
+    constructor(x,y,r){
+     
+      this.pow=createSprite(x,y,r,r)
+      this.pow.velocityX=-8
+      this.pow.velocityY=0
+        var rand=Math.round(random(1,4))
+        console.log(rand)
+        switch(rand){
+            case 1:
+                this.image=loadImage("power.png")
+                break
+                case 2:
+                    this.image=loadImage("power (2).png")
+                    break
+                    
+        }
+       this.pow.addImage(this.image)
+       
+      
+    }
+}
